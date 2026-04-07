349773a73f9899b119bbcd4a2c4cbf91c9a1178a
Kavindu Chamuditha <kavindustd@gmail.com>
2026-04-08 00:14:58 +0530

    Initialize Spring Boot project with Maven setup and basic configurations

---
+++ src/test/java/com/smart_campus_hub/smart_campus_api/SmartCampusApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_campus_hub.smart_campus_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartCampusApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
