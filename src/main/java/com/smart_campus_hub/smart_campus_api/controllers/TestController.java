add2baa76fa30807d916528fafbd9cc4c409ce5f
Kavindu Chamuditha <kavindustd@gmail.com>
2026-04-08 19:04:45 +0530

    feat: initialize backend project with CORS configuration, database properties, and test controller

--- src/main/java/com/smart_campus_hub/smart_campus_api/controllers/TestController.java
+++ src/main/java/com/smart_campus_hub/smart_campus_api/controllers/TestController.java
@@ -1,20 +1,5 @@
-
-
 package com.smart_campus_hub.smart_campus_api.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class TestController {
-
-    @GetMapping("/")
-    public String home() {
-        return "Smart Campus API is running 🚀";
-    }
-
-    @GetMapping("/test")
-    public String test() {
-        return "Backend is working correctly";
-    }
+    // Deprecated. Please refer to com.smart_campus_hub.smart_campus_api.controller.TestController
 }
